681af6264b523992eec5c09685aaed5ce2a40303
gabriellfe <87450585+gabriellfe@users.noreply.github.com>
2021-07-20 10:11:03 -0300

    Add files via upload

---
+++ cities-api-master/README.md
@@ -0,0 +1,2 @@
+# API Rest de consulta de cidades brasileiras e distância entre elas
+Java Porject - Digital Innovation One
---
+++ cities-api-master/src/main/java/com/github/rodrigo1901/citiesapi/states/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.rodrigo1901.citiesapi.states;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
